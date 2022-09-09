54eb0dde756d25453b98431e268c66016683b364
David Ewan Campbell <ewan.david@gmail.com>
2022-09-09 15:19:23 +0100

    added second passing test for multiple rolls

--- frame.test.js
+++ frame.test.js
@@ -1,10 +1,16 @@
 const Frame = require("./frame");
 
-describe('Frame', () => {
+describe("Frame", () => {
   describe('.addRoll', () => {
     it("Can receive a roll", () => {
       const frame = new Frame();
       frame.addRoll(5);
+    });
+
+    it("Can receive two rolls", () => {
+      const frame = new Frame();
+      frame.addRoll(5);
+      frame.addRoll(4);
     });
   });
 });
