c89e62fee89612a90120f39ab80bd93a7c178c4f
David Ewan Campbell <ewan.david@gmail.com>
2022-09-12 14:20:10 +0100

    test added to check for keeping score within each frame, failing as frame.score not a function as expected

--- frame.test.js
+++ frame.test.js
@@ -14,4 +14,12 @@ describe("Frame", () => {
       expect(frame.rolls.length).toEqual(2);
     });
   });
+
+  describe(".score", () => {
+    it('can score an incomplete frame', () => {
+      const frame = new Frame();
+      frame.addRoll(5);
+      expect(frame.score()).toEqual(5);
+    })
+  })
 });
